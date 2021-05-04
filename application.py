5d4215f2f2a17d4c2c94d2496c384e8b2704fe30
gina-cyber <77301888+gina-cyber@users.noreply.github.com>
2021-05-04 14:18:56 -0700

    Update application.py

--- application.py
+++ application.py
@@ -1,1 +1,24 @@
+# This is a guess the number game.
+import random
 
+class Guessing_Game:
+    def __init__(self):
+      print(“Welcome to Numbers Guessing Game!!!”)
+      solution = random.randint(1,10)
+    
+      Number_Of_Guess = 5
+      Attempts=0
+    
+      while Attempts < Number_Of_Guess:
+        Attempts=Attempts+1
+        print("Take a guess at a number between 1 and 10:") 
+        guess = int(input())
+    
+        if guess < solution:
+          print("try guessing higher")
+        elif guess > solution:
+          print("try guessing  lower")
+        else:
+          print(f'Good job. It took {Attempts} attempts!') 
+
+Guessing_Game()
