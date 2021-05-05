1c7a2854943e69d76165eb1266dc0c7193508a8b
gina-cyber <77301888+gina-cyber@users.noreply.github.com>
2021-05-05 11:56:48 -0700

    Update application.py

--- application.py
+++ application.py
@@ -3,22 +3,37 @@ import random
 
 class Guessing_Game:
     def __init__(self):
-      print(“Welcome to Numbers Guessing Game!!!”)
+      print("Welcome to Numbers Guessing Game!!!")
       solution = random.randint(1,10)
-    
+   
       Number_Of_Guess = 5
-      Attempts = 0
-    
+      Attempts=0
+      got_answer=False
+   
       while Attempts < Number_Of_Guess:
+        print("Take a guess at a number between 1 and 10:")
+        try:
+          guess = int(input())
+          if (guess < 1 or guess > 10):
+            print("Must enter nubmer between 1 and 10")
+            continue
+        except ValueError:
+          print("Must enter a whole number:")
+          continue
+   
         Attempts=Attempts+1
-        print("Take a guess at a number between 1 and 10:") 
-        guess = int(input())
-    
         if guess < solution:
           print("try guessing higher")
         elif guess > solution:
-          print("try guessing lower")
+          print("try guessing  lower")
         else:
-          print(f'Good job. It took {Attempts} attempts!') 
+          print(f'Good job. It took {Attempts} attempts!')
+          got_answer=True
+          break
 
-Guessing_Game()
+      if (got_answer==False):
+        print(f'Sorry, you guessed the max times {Attempts}. Try playing again')
+
+if __name__ == "__main__":
+
+    Guessing_Game()
