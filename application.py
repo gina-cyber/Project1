ea2a813968b62f0d6dd042df10e670061b4e0ded
gina-cyber <77301888+gina-cyber@users.noreply.github.com>
2021-05-05 12:00:43 -0700

    Update application.py

--- application.py
+++ application.py
@@ -3,37 +3,37 @@ import random
 
 class Guessing_Game:
     def __init__(self):
-      print("Welcome to Numbers Guessing Game!!!")
-      solution = random.randint(1,10)
+        print("Welcome to Numbers Guessing Game!!!")
+        solution = random.randint(1,10)
    
       Number_Of_Guess = 5
-      Attempts=0
-      got_answer=False
+      Attempts = 0
+      got_answer = False
    
       while Attempts < Number_Of_Guess:
-        print("Take a guess at a number between 1 and 10:")
+          print("Take a guess at a number between 1 and 10:")
         try:
-          guess = int(input())
-          if (guess < 1 or guess > 10):
+            guess = int(input())
+            if (guess < 1 or guess > 10):
             print("Must enter nubmer between 1 and 10")
             continue
-        except ValueError:
-          print("Must enter a whole number:")
-          continue
+            except ValueError:
+            print("Must enter a whole number:")
+            continue
    
-        Attempts=Attempts+1
+        Attempts = Attempts+1
         if guess < solution:
-          print("try guessing higher")
+            print("try guessing higher")
         elif guess > solution:
-          print("try guessing  lower")
+            print("try guessing  lower")
         else:
-          print(f'Good job. It took {Attempts} attempts!')
-          got_answer=True
-          break
+            print(f'Good job. It took {Attempts} attempts!')
+            got_answer=True
+            break
 
       if (got_answer==False):
-        print(f'Sorry, you guessed the max times {Attempts}. Try playing again')
+          print(f'Sorry, you guessed the max times {Attempts}. Try playing again')
 
 if __name__ == "__main__":
 
-    Guessing_Game()
+      Guessing_Game()
