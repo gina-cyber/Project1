b6b5ba9da024b2cde1d45b1b6a8e0edd23ab783c
gina-cyber <77301888+gina-cyber@users.noreply.github.com>
2021-05-05 21:40:39 -0700

    Update application.py

--- application.py
+++ application.py
@@ -1,24 +1,23 @@
 # This is a guess the number game.
 import random
 
-def main:
-        print("Welcome to Numbers Guessing Game!!!")
-        solution = random.randint(1,10)
+def start_game():
+    print("Welcome to Numbers Guessing Game!!!")
+    solution = random.randint(1,10)
    
-      Number_Of_Guess = 5
-      Attempts = 0
-      got_answer = False
-   
-      while Attempts < Number_Of_Guess:
-          print("Take a guess at a number between 1 and 10:")
+    Number_Of_Guess = 5
+    Attempts = 0
+    got_answer = False
+    while Attempts < Number_Of_Guess:
+        print("Take a guess at a number between 1 and 10:")
         try:
             guess = int(input())
             if (guess < 1 or guess > 10):
-            print("Must enter nubmer between 1 and 10")
-            continue
-            except ValueError:
-            print("Must enter a whole number:")
-            continue
+                print("Must enter nubmer between 1 and 10")
+                continue
+        except ValueError:
+         print("Must enter a whole number:")
+         continue
    
         Attempts = Attempts+1
         if guess < solution:
@@ -29,10 +28,10 @@ def main:
             print(f'Good job. It took {Attempts} attempts!')
             got_answer=True
             break
-
-      if (got_answer==False):
-          print(f'Sorry, you guessed the max times {Attempts}. Try playing again')
+        if (got_answer==False):
+           print(f'Sorry, you guessed the max times {Attempts}. Try playing again')
 
 if __name__ == "__main__":
 
-     
+      def end_game():
+          Print("Game ends here.")
